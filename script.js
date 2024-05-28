afbd43f99f76bdc92edd64cfabab27d8d5beab9c
LACOUR Alexandre <lacour_alexandre@proton.me>
2024-05-28 11:52:25 +0200

    api function working, purifyed by regexes on the input

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    let ip;
+    let input;
     let ipOutput = document.getElementById('ipOutput');
     let ipInput = document.getElementById('ipInput');
     let submitBtn = document.getElementById('submit-btn');
@@ -7,10 +7,20 @@ document.addEventListener('DOMContentLoaded', (event) => {
     let locationInfo = document.getElementById('location');
     let isp = document.getElementById('isp');
 
-    function apiCall(ip) {
+    function apiCall(input) {
         let url = `https://geo.ipify.org/api/v1?apiKey=at_N7oX6kM2kWRfmFPIS5gDCQrCeOSqE`;
-        if(ip) {
-            url += `&ipAddress=${ip}`;
+        let ipRegex = new RegExp(/^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$|^(?:[A-Fa-f0-9]{1,4}:){7}[A-Fa-f0-9]{1,4}$/);
+        let domainRegex = new RegExp(/^(?:[a-z]+\.)?[a-z]+\.[a-z]+$/);
+        if(input === "" || input === null || input === undefined) {
+           url += `&ipAddress=`
+        } else if(ipRegex.test(input)) {
+            console.log('ip')
+            url += `&ipAddress=${encodeURIComponent(input)}`;
+        } else if(domainRegex.test(input)){
+            console.log('domain')
+            url += `&domain=${encodeURIComponent(input)}`;
+        } else {
+            alert('Invalid input');
         }
         fetch(url)
             .then(response => response.json())
@@ -25,7 +35,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
             })
             .catch(error => console.log(error));
     }
-    apiCall(ip);
+    apiCall(input);
     submitBtn.addEventListener('click', (event) => {
         console.log('clicked')
         ipOutput.textContent = "";
@@ -33,9 +43,10 @@ document.addEventListener('DOMContentLoaded', (event) => {
         timezone.textContent = "";
         locationInfo.textContent = "";
         if(event.target === submitBtn) {
-            ip = ipInput.value;
-            apiCall(ip);
-            console.log(ip);
+            input = ipInput.value;
+            apiCall(input);
+            console.log(input);
         }
     });
+
 });
