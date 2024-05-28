3930122f849e1797c8397b70a60919f9aaa2638e
LACOUR Alexandre <lacour_alexandre@proton.me>
2024-05-28 15:46:13 +0200

    add localization from leaflet lib

--- script.js
+++ script.js
@@ -6,7 +6,19 @@ document.addEventListener('DOMContentLoaded', (event) => {
     let timezone = document.getElementById('timezone');
     let locationInfo = document.getElementById('location');
     let isp = document.getElementById('isp');
-
+    let loclat;
+    let loclong;
+    var map = L.map('map').setView([51.505, -0.09], 13);
+    var persIcon = L.icon({
+        iconUrl: './images/icon-location.svg',
+        
+    
+        iconSize:     [38, 50], // size of the icon
+        shadowSize:   [50, 64], // size of the shadow
+        iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+        shadowAnchor: [4, 62],  // the same for the shadow
+        popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+    });
     function apiCall(input) {
         let url = `https://geo.ipify.org/api/v1?apiKey=at_N7oX6kM2kWRfmFPIS5gDCQrCeOSqE`;
         let ipRegex = new RegExp(/^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$|^(?:[A-Fa-f0-9]{1,4}:){7}[A-Fa-f0-9]{1,4}$/);
@@ -15,6 +27,8 @@ document.addEventListener('DOMContentLoaded', (event) => {
         isp.textContent = "";
         timezone.textContent = "";
         locationInfo.textContent = "";
+        loclong = "";
+        loclat = "";
         if(input === "" || input === null || input === undefined) {
            url += `&ipAddress=`
         } else if(ipRegex.test(input)) {
@@ -27,11 +41,17 @@ document.addEventListener('DOMContentLoaded', (event) => {
         fetch(url)
             .then(response => response.json())
             .then(data => {
+                console.log(data);
                 ipOutput.textContent= `${data.ip}`;
                 ipInput.value = `${data.ip}`;
                 isp.textContent = `${data.isp}`;
                 timezone.textContent = `UTC${data.location.timezone}`;
                 locationInfo.textContent = `${data.location.city}, ${data.location.region} ${data.location.postalCode}`;
+                loclong = data.location.lng;
+                loclat = data.location.lat; 
+                let newLat = loclat + 0.25 * (map.getBounds().getNorth() - map.getBounds().getSouth());
+                map.setView([newLat, loclong], 13);
+                L.marker([loclat, loclong], {icon: persIcon}).addTo(map);
             })
             .catch(error => console.log(error));
     }
@@ -42,5 +62,9 @@ document.addEventListener('DOMContentLoaded', (event) => {
             apiCall(input);
         }
     });
-
+    
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
 });
