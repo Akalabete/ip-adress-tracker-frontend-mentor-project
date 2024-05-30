d37edbbcf288731a31159657ee2ebe76d8252b9e
LACOUR Alexandre <lacour_alexandre@proton.me>
2024-05-30 09:51:49 +0200

    add an input field reset on click

--- script.js
+++ script.js
@@ -63,7 +63,11 @@ document.addEventListener('DOMContentLoaded', (event) => {
             apiCall(input);
         }
     });
-    
+
+    ipInput.addEventListener('click', (event) => {
+        event.preventDefault();
+        ipInput.value = "";
+    });
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
